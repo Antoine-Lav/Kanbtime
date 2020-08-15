f96dd4731a02d132690dbdb02290a9fe0a06da27
antoinelavie <antoinelavie.pro@gmail.com>
2020-08-15 23:13:48 +0200

    KANBT : Project installation

---
+++ src/test/java/com/perso/kanbtime/KanbtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perso.kanbtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbtimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
